86d35fbb640a003c62b33078c9d9a3294d74af5e
NastyaLisa <118108923+NastyaLisa@users.noreply.github.com>
2023-03-01 15:49:27 +0200

    Update task_4.js
    answer from lector

--- js/task_4.js
+++ js/task_4.js
@@ -25,3 +25,29 @@ let admin = new Admin("Mike", "MK_18", 18, true);
 
 console.log(admin.getUserName(admin));
 
+
+клас адмін має приймати юзера, у якого викликається метод який повертає ім"я
+
+class Admin extends User{
+
+  #isAdmin
+
+  constructor(name, login, age){
+
+    super(name, login, age)
+
+    this.#isAdmin = true
+
+  }
+
+  getUserName(user){
+
+    user.getName(this.#isAdmin)
+
+  }
+
+}
+
+let admin = new Admin('Bob', 'God', 35);
+
+admin.getUserName(user1);
